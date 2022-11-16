46f62763f899efeb84c512f30dbb45d649d1d76a
David Resanto <david@gbaligatra.com>
2022-11-16 17:28:23 +0800

    - Add no shipping preference

--- app/Http/Controllers/PaypalGatewayController.php
+++ app/Http/Controllers/PaypalGatewayController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use MoneyCurrency;
 use Srmklive\PayPal\Services\PayPal as PayPalClient;
 
 class PaypalGatewayController extends Controller
@@ -20,17 +21,23 @@ class PaypalGatewayController extends Controller
     public function createOrder(Request $request)
     {
         $data = json_decode($request->getContent(), true);
+
+        $priceIDR = 100000;
+        $priceUSD = MoneyCurrency::IDRToUSD($priceIDR);
         $dataOrder = [
             "intent" => "CAPTURE",
             "purchase_units" => [
                 [
                     "amount" => [
                         "currency_code" => env('PAYPAL_CURRENCY'),
-                        "value" => $data['amount'],
+                        "value" => $priceUSD,
                     ],
                     "description" => "Purchase Of Human Race Dream Project 2022 #001"
                 ],
             ],
+            'application_context' => [
+                'shipping_preference' => 'NO_SHIPPING',
+           ]
         ];
 
         $order = $this->provider->createOrder($dataOrder);
